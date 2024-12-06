6827ecb7e94ed078e0f4f7c7d4904c36b1a59e9a
kalcar <kalievcarrier@gmail.com>
2024-12-06 14:49:47 -0800

    added sets and set operations

--- python.py
+++ python.py
@@ -120,7 +120,53 @@ if (dictionaryFunctions):
     myDictionary.get(100, len) # returns value for key (1st arg), else default value (2nd arg)
 
 # Python has sets
-mySet = {"my", "set"}
-myEmptySet = set()
-myEmptyDictionary = {} #!!!!
+# Sets are implemented similar to dicts, and are performant
+setOperations = False
+if (setOperations):
+    mySet = {"my", "set"}
+    myEmptySet = set()
+    myEmptyDictionary = {} # !!!!
+
+    mySet1 = {1,2,3,4,5,6}
+    mySet2 = {5,6,7,8,9,10}
+
+    # Checking for membership in a set
+    print( 1 in mySet1) # True. 
+    # Speed: O(1) on average, O(n) worst case 
+
+    # adding an element. Changes the set inplace. 
+    mySet1.add(7) 
+    # Speed: O(1) on average, O(n) worst case
+    # print(mySet1) # {1, 2, 3, 4, 5, 6, 7}
+
+    # Union, merging two sets. duplicates removed. Returns a new set
+    # print(mySet1 | mySet2) # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    # print(mySet1.union(mySet2))
+    # Speed: O(len(s1) + len(s2))
+
+    # Intersection, outputs common elements. Returns a new set
+    print(mySet1 & mySet2) # {5, 6, 7}
+    print(mySet1.intersection(mySet2))
+    # mySet1 &= mySet2  # this changes mySet1 inplace to the intersection
+    # print(mySet1.intersection_update(mySet2))
+    # print(mySet1, mySet2) # {5, 6, 7} {5, 6, 7, 8, 9, 10}
+    # Speed: O(min(len(s1), len(s2))), worst case O(len(s1)*(len(s2)))
+    # Multiple Intersection (s1 & s2 & sN) Speed: (N-1)*O(max(len(s1, s2, sN)))
+
+    # Difference, outputs elements in A not in B. Returns a new set
+    # NOTE: Does not include elements in B not in A
+    print(mySet1.difference(mySet2)) # {1, 2, 3, 4}
+    print(mySet1 - mySet2 ) # {1, 2, 3, 4}
+    # Speed: O(len(s1))
+
+
+    # Symmetric Difference, outputs elements that are not in common
+    print(mySet1.symmetric_difference(mySet2))
+    print(mySet1 ^ mySet2)
+    # Speed: O(len(s1)). Worst case O(len(s1)*len(s2))
+    # edits the set inplace
+    # print(mySet1.symmetric_difference_update(mySet2))
+    # mySet1 ^= mySet2
+    # Speed: O(len(s2)). Worse case O(len(s2)*len(s3))
+
 
